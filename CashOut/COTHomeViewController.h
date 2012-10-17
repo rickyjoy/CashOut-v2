4116550a74d810853df6a3e6d7144b912226c6e1
rickyjoy <rickyjoy@live.com>
2012-10-16 19:34:37 -0700

    skeleton

---
+++ CashOut/COTHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  COTHomeViewController.h
+//  CashOut
+//
+//  Created by Rick on 10/16/12.
+//  Copyright (c) 2012 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COTHomeViewController : UIViewController
+
+@end
---
+++ CashOut/COTHomeViewController.m
@@ -0,0 +1,50 @@
+//
+//  COTHomeViewController.m
+//  CashOut
+//
+//  Created by Rick on 10/16/12.
+//  Copyright (c) 2012 Rick. All rights reserved.
+//
+
+#import "COTHomeViewController.h"
+
+@interface COTHomeViewController ()
+
+@end
+
+@implementation COTHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
+    [super viewWillDisappear:animated];
+}
+
+@end
